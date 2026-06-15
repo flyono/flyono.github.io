a2f40182df259d9067bbdc1c2f5ac7d1c7ee4934
Flyone <1508948470@qq.com>
2026-06-15 18:27:00 +0800

    feat: 更新Godot和Unity文档元数据，添加图标并修改标题

--- app/docs/layout.tsx
+++ app/docs/layout.tsx
@@ -1,28 +1,10 @@
 import { baseOptions } from '@/lib/layout.shared';
 import { source } from '@/lib/source';
 import { DocsLayout } from 'fumadocs-ui/layouts/docs';
-import type { LayoutTab } from 'fumadocs-ui/layouts/shared';
-import { Component } from 'lucide-react';
-import { GodotIcon } from '@/components/icons';
-
-const docsTabs: LayoutTab[] = [
-  {
-    title: 'Godot 笔记',
-    description: 'Godot\'s Docs',
-    url: '/docs/godot',
-    icon: <GodotIcon strokeWidth={1} />,
-  },
-  {
-    title: 'Unity',
-    description: 'Unity 学习相关文档',
-    url: '/docs/unity',
-    icon: <Component />,
-  }
-];
 
 export default function Layout({ children }: LayoutProps<'/docs'>) {
   return (
-    <DocsLayout tree={source.getPageTree()} tabs={docsTabs} tabMode="auto" {...baseOptions()}>
+    <DocsLayout tree={source.getPageTree()} {...baseOptions()}>
       {children}
     </DocsLayout>
   );
